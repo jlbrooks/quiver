469834e30ac9b79ad4eb6e84b0dee43713745593
Jacob <jacob@chrisbrooks.org>
2014-06-13 22:02:42 -0400

    refactored the posts spec to only create it once

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
+  before do
+    @post = FactoryGirl.create(:post)
+  end
+
   it "should have a valid factory" do
-    FactoryGirl.create(:post).should be_valid
+    @post.should be_valid
   end
   it "should be invalid without a title" do
-    FactoryGirl.build(:post, :title => nil).should_not be_valid
+    @post.title = nil
+    @post.should_not be_valid
   end
   it "should be invalid without a body" do
-    FactoryGirl.build(:post, :body => nil).should_not be_valid
+    @post.body = nil
+    @post.should_not be_valid
   end
 end
