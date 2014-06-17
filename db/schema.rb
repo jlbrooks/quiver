adf4b1561ff176d8478e4dccd6a75ec3c5fac612
Connor Carraher <connor@carraher.com>
2014-06-16 18:39:30 -0700

    Add path index for comments (Needs fix)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140614192802) do
+ActiveRecord::Schema.define(version: 20140617003456) do
+
+  create_table "comments", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "body"
+    t.integer  "post_id"
+  end
 
   create_table "groups", force: true do |t|
     t.string   "name"
