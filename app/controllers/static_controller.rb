90e50881c9d1c5bd1129fbcbf3644c0a810c726c
Jacob <jacob@chrisbrooks.org>
2014-06-29 23:56:00 -0400

    excerpts on static home page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,5 +3,13 @@ class StaticController < ApplicationController
   #GET /home
   def home
     @posts = Post.all
+    create_excerpts(50)
+  end
+
+  private
+  def create_excerpts(words)
+    for post in @posts
+      post.excerpt = post.body.split[0...words].join(' ') + '...'
+    end
   end
 end
